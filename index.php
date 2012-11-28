e864f02618fd1a504ab2daea7d1802bcf588fbc6
RadioCyber FM <drie88@myself.com>
2012-11-28 15:31:21 +0700

    @@ -262,16 +262,16 @@ $app_name = idx($app_info, 'name', '');              foreach ($app_using_friends as $auf) {                // Extract the pieces of info we need from the requests above                $id = idx($auf, 'uid');                $name = idx($auf, 'name');            ?> -          <li> +              <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">                <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">                <?php echo he($name); ?>              </a> -          </li> +            <?php              }            ?>          </ul>        </div>

--- index.php
+++ index.php
@@ -264,12 +264,12 @@ $app_name = idx($app_info, 'name', '');
               $id = idx($auf, 'uid');
               $name = idx($auf, 'name');
           ?>
-          <li>
+          
             <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
               <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
               <?php echo he($name); ?>
             </a>
-          </li>
+         
           <?php
             }
           ?>
